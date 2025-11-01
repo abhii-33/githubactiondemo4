84f82f277eb23cd8a3bde5f83a86a872a25c50eb
Bhushan Shelar <shelarbhushan26@gmail.com>
2025-11-01 18:41:37 +0530

    testing1

--- read_data_api.py
+++ read_data_api.py
@@ -1,9 +1,13 @@
 import requests
 import pandas as pd
 
-response = requests.get("https://jsonplaceholder.typicode.com/users")
-data = response.json()
-df = pd.DataFrame(data)
-df = df[["id", "name"]]
+token = os.getenv("API_TOKEN")
+print(f"Token : {token}")
 
-print(df)
+# response = requests.get("https://jsonplaceholder.typicode.com/users")
+
+# data = response.json()
+# df = pd.DataFrame(data)
+# df = df[["id", "name"]]
+
+# print(df)
